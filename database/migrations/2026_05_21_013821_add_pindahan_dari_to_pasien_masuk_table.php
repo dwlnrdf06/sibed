38665b9a8470cfc568c40bc286b75c203c932dd1
dwlnrdf06 <dwlnrdf06@gmail.com>
2026-05-21 08:40:38 +0700

    Update 2026_05_21_013821_add_pindahan_dari_to_pasien_masuk_table.php

--- database/migrations/2026_05_21_013821_add_pindahan_dari_to_pasien_masuk_table.php
+++ database/migrations/2026_05_21_013821_add_pindahan_dari_to_pasien_masuk_table.php
@@ -20,7 +20,34 @@ return new class extends Migration
      * Reverse the migrations.
      */
     public function down(): void
+    {<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::table('pasien_masuk', function (Blueprint $table) {
+        $table->string('pindahan_dari')->nullable();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
+        Schema::table('pasien_masuk', function (Blueprint $table) {
+            //
+        });
+    }
+};
         Schema::table('pasien_masuk', function (Blueprint $table) {
             //
         });
